6c7e5d3e53cba1eab4e09593d05d793236395f96
Chyld Medford <chyld.medford@gmail.com>
2015-05-02 09:58:38 -0700

    twitter

--- angular/client/config/constants.js
+++ angular/client/config/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 
-angular.module('poseidon')
-.constant('firebaseUrl', 'https://ENTER-YOUR-FB-URL.firebaseio.com/');
+angular.module('facebook')
+.constant('firebaseUrl', 'https://facebook-x.firebaseio.com/');
